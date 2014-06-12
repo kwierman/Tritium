370072677b26b20c56b2df5f47afd8707a136d44
Kevin Wierman <kwierman@gmail.com>
2014-06-12 11:58:00 -0400

    Cleaning Up Design

--- Design/inc/TriNullTypes.h
+++ Design/inc/TriNullTypes.h
@@ -3,25 +3,16 @@
 
 namespace TritiumDesign{
 
-///Dummy Null Class
-struct TriNullType{};
-class TriEmptyType {};
+    ///Dummy Null Class
+    struct TriNullType{};
+    class TriEmptyType {};
+        
     
-    
-    inline bool operator==(const TriEmptyType&, const TriEmptyType&)
-    {
-        return true;
-    }   
+    inline bool operator==(const TriEmptyType&, const TriEmptyType&){return true;}   
 
-    inline bool operator<(const TriEmptyType&, const TriEmptyType&)
-    {
-        return false;
-    }
-    
-    inline bool operator>(const TriEmptyType&, const TriEmptyType&)
-    {
-        return false;
-    }
+    inline bool operator<(const TriEmptyType&, const TriEmptyType&){return false;}
+
+    inline bool operator>(const TriEmptyType&, const TriEmptyType&){return false;}
 
 }
 
---
+++ Design/inc/TriSequence.h
@@ -0,0 +1,5 @@
+#ifndef TriSqeuence_h_
+#define TriSequence_h_
+
+
+#endif //
