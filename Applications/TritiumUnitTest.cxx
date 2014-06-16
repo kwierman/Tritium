341ec45d3b50263a914df8c9d2bc5abb2a15b961
Kevin Wierman <kwierman@gmail.com>
2014-06-16 13:28:28 -0400

    Testing Updates

--- Applications/TritiumUnitTest.cxx
+++ Applications/TritiumUnitTest.cxx
@@ -11,19 +11,19 @@
 using namespace Tritium;
 int main(int argc, char* argv[]){
 
-	std::cout<<"Math Test?"<<std::endl;
-	std::cout<<dbl_epsilon<<std::endl;
-	std::cout<<std::setprecision(15)<<nat<<std::endl;
-	std::cout<<power(3.0,2.0)<<std::endl;
-	std::cout<<9-power(3.,2.)<<std::endl;
-	std::cout<<factorial(3)<<std::endl;
-	std::cout<<exponent_taylor(2,1000)<<std::endl;
-	std::cout<<exponent(2)<<std::endl;
-	std::cout<<log_taylor(2,10)<<std::endl;
-	std::cout<<sqrt_from_exp(2,1000)<<std::endl;
-	std::cout<<power(2.,0.5)<<std::endl;
+	std::cout<<"Math Test?"				<<std::endl;
+	std::cout<<dbl_epsilon				<<std::endl;
+	std::cout<<std::setprecision(15)	<<nat<<std::endl;
+	std::cout<<power(3.0,2.0)			<<std::endl;
+	std::cout<<9-power(3.,2.)			<<std::endl;
+	std::cout<<factorial(3)				<<std::endl;
+	std::cout<<exponent_taylor(2,1000)	<<std::endl;
+	std::cout<<exponent(2)				<<std::endl;
+	std::cout<<log_taylor(2,10)			<<std::endl;
+	std::cout<<sqrt_from_exp(2,1000)	<<std::endl;
+	std::cout<<power(2.,0.5)			<<std::endl;
 	
-	TriScalar scaler 	= 	TriRate(1.0)	*	TriTime(1.0);		//This works
+	TriScalar scaler 	= 	TriRate(   1.0)	*	TriTime(1.0);		//This works
 	TriAction act 		=	TriEnergy(20.0)	*	TriTime(5.0);		//Also works
 	//TriScalar doesntWork = TriTime(0.0)*TriTime(0.0); //This doesn't work
 
--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Tritium
 
 A tritium calculation engine
 
-Note: Still in code design. Engine does not build, only design components
+Note: Still in code design. Engine does not build, only design components.
+
+Note: Backwards compatibility will be broken on migration to Protium and Deuterium.
+
